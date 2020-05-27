67b91a4f5050f48440d789f1312d717328528ac0
Michael Neff <nef16009@byui.edu>
2020-05-27 11:50:31 -0500

    Update 5/26/2020
    - Finished the sqlite connect function
    - Added a test for the connect function

--- Connect_To_SQLite_Database.R
+++ Connect_To_SQLite_Database.R
@@ -1,31 +1,81 @@
-###  Connect_To_SQLite_Database                                                                                              
-###  (By Michael Neff)                                                                                              
+### Connect_To_SQLite_Database                                                                                              
 ###                                                                         
 ###                                                                                             
-###     This code uses the ODBC and RSQLite packages to connect to a SQLite Database                                                                                             
-###                                                                                                
-###     - A connection is created with the RSQLite package
-###     - That connection is used by the ODBC package to access and interact with the SQLite Database
+### This code uses the ODBC and RSQLite packages to connect to a SQLite Database                                                                                             
+###                                                                                            
+###   - A connection is created with the RSQLite package
+###   - That connection is used by the ODBC package to access and interact with the SQLite Database
 
 
-library(dplyr)
 
-library(RSQLite)  # Creates a connection ("conn") to the SQLite Database 
-                  #   (https://db.rstudio.com/databases/sqlite/)
 
-library(odbc)     # Implements the RSQLite "conn" to access the SQL Database and interact with it 
-                  #   (https://db.rstudio.com/odbc/)
-                
+library(RSQLite)      # Can used to create a connection to a SQLite3 Database
+                      # Also downloads DBI, which is used to interact with the database
+library(tidyverse)
+
+
+
+
+
+
+
+
+######################
+## DEFINE FUNCTIONS ##
+######################
+
+      
+
+
+
+
+
+
+create_sqlite_connection <- function(db_location, db_name) {
+  
+  ### Returns a connection to a SQLite Database file on computer
+  ###
+  ### Found working solution here:
+  ### https://stackoverflow.com/questions/32545777/r-connection-to-sqlite
+  
+  
+  # Set the working directory to the folder containing the SQLIte file
+  setwd(db_location)
+  
+  # Create the connection to the SQLite Database using RSQLite
+  conn <- dbConnect(dbDriver("SQLite"), db_name)
+  
+  # Return the connection
+  return(conn)
+}
+
+
+
+
+
+
+
+
+####################
+## TEST FUNCTIONS ##
+####################
+
+
+
+
+
+
 
 
 ## Test with the HoopStats SQLite Database
 
 
-# Create a connection with HoopStats
+# Create a connection to HoopStats with RSQLite
+conn <- create_sqlite_connection("C:\\Users\\mneff\\Desktop\\College Stuff\\Data Science Society\\HoopStats Project",
+                                 "HoopStats.sqlite")
 
-
-
-
+# To test the connection, list all tables in the database
+DBI::dbListTables(conn)
 
 
 
